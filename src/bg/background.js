dc38104a0f7ec2699d8033cb49f6327519872786
Jayant Arora <jaistudy1996@gmail.com>
2017-08-16 10:55:13 -0400

    clean up code and remove options page

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,20 +1,4 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-//TODO: add location refresh in settings.
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-
+// Global variables
 var ERROR = "error";
 var locationCords = {};
 var openWeatherMapKey = 'bad43f7b54535ae3baeb52cbe1beff28';
@@ -178,6 +162,7 @@ chrome.omnibox.onInputChanged.addListener(
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
     console.log('inputEntered: ' + text);
+    // Change units and refresh weather.
     if(["F", "f"].indexOf(text.replace("change units ")[0]) > -1){
       weatherUnits = "Imperial";
       getWeather(locationCords);
